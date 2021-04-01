38379e51318c1cd66f0c467bb42a680de01b3539
josemasf <josemasf@gmail.com>
2021-04-01 10:34:25 +0200

    init repos
    Add gridsome
    Call fake blog api

---
+++ src/pages/Blog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="edge in $page.allPosts.edges" :key="edge.node.id">
+      <h1 v-html="edge.node.title" />
+      <div v-html="edge.node.body"></div>
+    </div>
+  </div>
+</template>
+
+<page-query>
+
+  query {
+  allPosts {
+    edges {
+      node {
+        id
+        title
+        body
+      }
+    }
+  }
+}
+
+</page-query>
