1bfb71bc86dbd9bbf85cca8a64bd50423ff9e41f
josemasf <josemasf@gmail.com>
2021-04-01 10:44:07 +0200

    Add Layout
    Add blog link

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <Layout>
     <div v-for="edge in $page.allPosts.edges" :key="edge.node.id">
       <h1 v-html="edge.node.title" />
       <div v-html="edge.node.body"></div>
     </div>
-  </div>
+  </Layout>
 </template>
 
 <page-query>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,6 +13,7 @@
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
       <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+      <g-link to="/blog/">Blog</g-link>
     </p>
 
   </Layout>
